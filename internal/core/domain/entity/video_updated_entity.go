50849abd1de712d36d096f232e7a832f94324646
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-09-14 17:06:39 -0300

    refactor: Remove unused VideoUpdated entity definition

--- internal/core/domain/entity/video_updated_entity.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-import valueobject "github.com/FIAP-SOAT-G20/hackathon-video-service/internal/core/domain/value_object"
-
-type VideoUpdated struct {
-	VideoID uint64                  `json:"video_id"`
-	Status  valueobject.VideoStatus `json:"status"`
-	Hash    string                  `json:"hash"`
-}
